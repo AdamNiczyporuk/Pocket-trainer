f51aa342561ff58dd3e832e666cfe7ce5b22bb79
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-10 15:36:28 +0200

    Split Program to Function

--- Program.cs
+++ Program.cs
@@ -2,16 +2,15 @@
 using System;
 
 class Program
-{
-    static void Main()
+{ 
+    static void Panel()
     {
-        int consoleWidth = Console.WindowWidth;
         bool exit = false;
 
         while (!exit)
         {
             // Tworzenie prompta z opcjami menu
-                         
+
             var option = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
                     .Title("[yellow]Wybierz opcję:[/]")
@@ -22,7 +21,7 @@ class Program
                     })
                     .HighlightStyle(new Style(foreground: Color.DarkMagenta_1)));
 
-            // Obsługa wybranej opcji
+            // Obsługa wsybranej opcji
             switch (option)
             {
                 case "Opcja 1":
@@ -47,6 +46,52 @@ class Program
                 Console.ReadKey();
             }
         }
+    }
+    static void LoginMenu()
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            // Tworzenie prompta z opcjami menu
+
+            var option = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[yellow]Wybierz opcję:[/]")
+                    .PageSize(10)
+                    .WrapAround(true)// Maksymalna ilość opcji na jednej stronie
+                    .AddChoices(new[] {
+                        "Log IN", "Sign IN", "Exit"
+                    })
+                    .HighlightStyle(new Style(foreground: Color.DarkMagenta_1)));
+
+            // Obsługa wsybranej opcji
+            switch (option)
+            {
+                case "Log IN":
+                    AnsiConsole.MarkupLine("[green]Wybrałeś Opcję 1![/]");
+                    break;
+                case "Sign IN":
+                    AnsiConsole.MarkupLine("[green]Wybrałeś Opcję 2![/]");
+                    break;
+                case "Exit":
+                    AnsiConsole.MarkupLine("[red]Koniec programu.[/]");
+                    exit = true;
+                    break;
+            }
+
+            // Czekaj na naciśnięcie klawisza przed powrotem do menu, jeśli użytkownik nie wybrał wyjścia
+            if (!exit)
+            {
+                AnsiConsole.MarkupLine("[grey]Naciśnij dowolny klawisz, aby wrócić do menu...[/]");
+                Console.ReadKey();
+            }
+        }
+    }
+    static void Main()
+    {
+        LoginMenu();
+       
     }
 }
 
