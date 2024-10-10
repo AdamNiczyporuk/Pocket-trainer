305507ca960bcc8b5238e5d4026f8c6c4e7490fa
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-10 12:35:25 +0200

    Add Menu

--- Program.cs
+++ Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Spectre.Console;
+using System;
 
-namespace KCK_Project__Console_Pocket_trainer_
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int consoleWidth = Console.WindowWidth;
+        bool exit = false;
+
+        while (!exit)
         {
+            // Tworzenie prompta z opcjami menu
+            var option = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[yellow]Wybierz opcję:[/]")
+                    .PageSize(10)
+                    .WrapAround(true)// Maksymalna ilość opcji na jednej stronie
+                    .AddChoices(new[] {
+                        "Opcja 1", "Opcja 2", "Opcja 3", "Wyjście"
+                    })
+                    .HighlightStyle(new Style(foreground: Color.DarkMagenta_1)));
+
+            // Obsługa wybranej opcji
+            switch (option)
+            {
+                case "Opcja 1":
+                    AnsiConsole.MarkupLine("[green]Wybrałeś Opcję 1![/]");
+                    break;
+                case "Opcja 2":
+                    AnsiConsole.MarkupLine("[green]Wybrałeś Opcję 2![/]");
+                    break;
+                case "Opcja 3":
+                    AnsiConsole.MarkupLine("[green]Wybrałeś Opcję 3![/]");
+                    break;
+                case "Wyjście":
+                    AnsiConsole.MarkupLine("[red]Koniec programu.[/]");
+                    exit = true;
+                    break;
+            }
+
+            // Czekaj na naciśnięcie klawisza przed powrotem do menu, jeśli użytkownik nie wybrał wyjścia
+            if (!exit)
+            {
+                AnsiConsole.MarkupLine("[grey]Naciśnij dowolny klawisz, aby wrócić do menu...[/]");
+                Console.ReadKey();
+            }
         }
     }
 }
