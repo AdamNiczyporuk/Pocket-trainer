2d9ffe3c427df00831e17623e569f76f667138e5
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-16 16:08:17 +0200

    Add Validation TO LogIN

--- Views/LogIN.cs
+++ Views/LogIN.cs
@@ -1,6 +1,9 @@
-﻿using Spectre.Console;
+﻿using KCK_Project__Console_Pocket_trainer_.Data;
+using KCK_Project__Console_Pocket_trainer_.Repositories;
+using Spectre.Console;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,14 @@ namespace KCK_Project__Console_Pocket_trainer_.Views
     {
         public static async Task Execute()
         {
+
+            var context = new ApplicationDbContext();
+            var userRepository = new UserRepository(context);
+            Console.Clear();
             while (true)
             {
                 // Wyświetlenie opcji logowania
+                AnsiConsole.MarkupLine("To LogIn enter username and password");
                 var username = AnsiConsole.Ask<string>("Enter your [yellow]Username[/]:");
                 var password = AnsiConsole.Prompt(
                     new TextPrompt<string>("Enter your [yellow]Password[/]:")
@@ -31,27 +39,43 @@ namespace KCK_Project__Console_Pocket_trainer_.Views
                 if (option == "Exit")
                 {
                     AnsiConsole.MarkupLine("[red]Exiting the application...[/]");
+                    await StartMenu.Execute();
                     break; // Wyjście z pętli i zakończenie aplikacji
                 }
+                if (option == "Login")
+                {
+                    var existingUser = userRepository.GetUserByUserName(username);
+                    if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+                    {
+                        AnsiConsole.MarkupLine("[red]Invalid username or password![/]");
+                        await LogIN.Execute();
+                    }
+                    if (existingUser != null)
+                    {
+                        if (existingUser.Password == password)
+                        {
+                            await Panel.Execute();
+                            break;
+                        }
+                        else
+                        {
+                            AnsiConsole.MarkupLine("[red]Invalid username or password![/]");
+                            await LogIN.Execute();
+                        }
+                    }
+                    else
+                    {
+                        AnsiConsole.MarkupLine("[red]User does not exist![/]");
+                        await SignIN.Execute();
+                    }
 
-                // Symulacja walidacji logowania (możesz zastąpić to własną logiką)
-                if (ValidateLogin(username, password))
-                {
-                    await Panel.Execute();
-                    break;
-                }
-                else
-                {
-                    AnsiConsole.MarkupLine("[red]Invalid username or password![/]");
+
                 }
             }
-        }
 
-        // Metoda symulująca walidację logowania
-        private static bool ValidateLogin(string username, string password)
-        {
-            // Przykładowa walidacja - zastąp to własną logiką
-            return username == "admin" && password == "admin";
+
+
         }
     }
 }
+
