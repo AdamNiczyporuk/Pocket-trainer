7927ac5d33353b0833e1d9015849300412c4486e
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-16 15:56:36 +0200

    Validation in SignIN

--- Views/SignIN.cs
+++ Views/SignIN.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using KCK_Project__Console_Pocket_trainer_.Views;
+using KCK_Project__Console_Pocket_trainer_.Models;
+using KCK_Project__Console_Pocket_trainer_.Data;
+using KCK_Project__Console_Pocket_trainer_.Repositories;
+
 
 namespace KCK_Project__Console_Pocket_trainer_.Views
 {
@@ -12,9 +16,10 @@ namespace KCK_Project__Console_Pocket_trainer_.Views
     {
         public static async Task Execute()
         {
+            var context = new ApplicationDbContext();
+            var userRepository = new UserRepository(context);
             while (true)
             {
-                var mail = AnsiConsole.Ask<string>("Enter your [yellow]E-mail[/]:");
                 var username = AnsiConsole.Ask<string>("Enter your [yellow]Username[/]:");
                 var password = AnsiConsole.Prompt(
                                new TextPrompt<string>("Enter your [yellow]Password[/]:")
@@ -35,25 +40,40 @@ namespace KCK_Project__Console_Pocket_trainer_.Views
                 }
                 if (option == "SignIN")
                 {
-                    if (mail == "" || username == "" || password == "")
+                    var existingUser = userRepository.GetUserByUserName(username);
+                    if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
                     {
                         AnsiConsole.MarkupLine("[red]Invalid username or password![/]");
                         await SignIN.Execute();
                     }
-                    else if (mail.Contains("@") == false)
+                    else if(existingUser != null)
                     {
-                        AnsiConsole.MarkupLine("[red]Invalid email![/]");
-                        await SignIN.Execute();
+                        AnsiConsole.MarkupLine("[red]User already exists!LogIN!!!![/]");
+                        await LogIN.Execute();
+                        break;
                     }
-                    else
+                    else if (existingUser != null)
                     {
                         // Check if the user already exists
                         // Seraching user thorugh Mail a
                         //Validate password and Username
                         // Add the user to the database
                         // AddUserToDatabase(mail, username, password);
-                        AnsiConsole.MarkupLine("[green]User added successfully![/]");
+                        AnsiConsole.MarkupLine("[red]User already exists![/]");
+                        await SignIN.Execute();
+                    }
+                    else
+                    {
+                        var newUser = new User
+                        {
+                            UserName = username,
+                            Password = password
+                        };
+                        userRepository.Add(newUser);
                         await Panel.Execute();
+                        break;
+
+
                     }
 
                 }
