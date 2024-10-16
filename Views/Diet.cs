140bd0f17e0676a845269ae3c63c8976d268abe7
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-16 15:29:49 +0200

    Add Diet.cs

---
+++ Views/Diet.cs
@@ -0,0 +1,35 @@
+﻿using CHATAPI;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCK_Project__Console_Pocket_trainer_.Views
+{
+    public class Diet
+    {
+        public static async Task Execute()
+        {
+
+            ChatGPT_diet.SetUpSetting();
+            String Prompt = "My weigh=87kg,Height=186cm,TrainingsPerWeek=6trainingPerWeek.Write me a diet plan for 7 seven days.";
+
+
+
+            var responseTask = ChatGPT_diet.SendRequestToChatGPT(Prompt);
+
+            Console.CursorVisible = false;
+            AnsiConsole.MarkupLine("[bold green]Generating Diet....[/]");
+
+            var response = await responseTask;
+
+            Console.CursorVisible = true;
+            AnsiConsole.Clear();
+            AnsiConsole.MarkupLine("[bold green]Diet Plan:[/]");
+            AnsiConsole.MarkupLine(response);
+
+        }
+    }
+}
