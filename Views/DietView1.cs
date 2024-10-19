f1c6b4fe83895a2bc6d0d27ed13ec46f69dddd6d
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-19 18:40:50 +0200

    Creating Two functions GetOptionDeitNotExist and GetOptionDietExist

--- Views/DietView1.cs
+++ Views/DietView1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,21 @@ namespace KCK_Project__Console_Pocket_trainer_.Views
 {
     public class DietView1
     {
-        public static string GetOption1()
+        public static string GetOptionDietNotExist()
+        {
+            var option = AnsiConsole.Prompt(new SelectionPrompt<string>().Title("[Green]Chose an option[/]")
+                .AddChoices(new[] { "Save Diet", "Generate Again", "Exit" })
+                .HighlightStyle(new Style(foreground: Color.Aqua)));
+            return option;
+        }
+
+        public static string GetOptionDietExist()
+        {
+            var option = AnsiConsole.Prompt(new SelectionPrompt<string>().Title("[Green]Chose an option[/]")
+                            .AddChoices(new[] { "Generate Again", "Exit" })
+                            .HighlightStyle(new Style(foreground: Color.Aqua)));
+            return option;
+        }
 
     }
 }
