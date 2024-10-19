bf15ae94ed7a87a96686a5fb33a6eabad1d26ca9
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-10-19 18:31:31 +0200

    Add Dietcontroller

---
+++ Controllers/DietViewController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCK_Project__Console(Pocket_trainer).Controllers
+{
+    internal class DietViewController
+{
+}
+}
